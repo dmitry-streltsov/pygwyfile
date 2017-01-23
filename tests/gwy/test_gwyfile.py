a7a269f92dacd4afa33d3f67af83ff98f5da61ae
Dmitry Streltsov <streltsov.dmitry@gmail.com>
2017-01-23 23:21:48 +0300

    Small fixes it unit tests

--- tests/gwy/test_gwyfile.py
+++ tests/gwy/test_gwyfile.py
@@ -4,14 +4,13 @@ from unittest.mock import Mock, patch, ANY
 from gwydb.gwy.gwyfile import read_gwyfile
 
 
-class Read_gwyTestCase(unittest.TestCase):
-    """Test read_gwyfile function"""
+class Func_read_gwy_TestCase(unittest.TestCase):
+    """Test read_gwyfile function in gwydb.gwy.gwyfile"""
 
     @patch('gwydb.gwy.gwyfile.os')
-    def test_raising_exception(self,
-                               mock_os):
-        """Test that OSError exception raises
-           if gwy file does not exists
+    def test_raise_exception_if_file_doesnt_exist(self, mock_os):
+        """Raise OSError exception if file
+           does not exist
         """
 
         filename = 'test.gwy'
@@ -20,19 +19,17 @@ class Read_gwyTestCase(unittest.TestCase):
 
     @patch('gwydb.gwy.gwyfile.os')
     @patch('gwydb.gwy.gwyfile.lib')
-    def test_transfer_of_function_args(self,
-                                       mock_libgwyfile,
-                                       mock_os):
-        """Test arguments transfer to gwyfile_read_file function"""
+    def test_passing_of_function_args(self,
+                                      mock_libgwyfile,
+                                      mock_os):
+        """If file exists call gwyfile_read_file function.
+           Check arguments passed to this function
+        """
 
         filename = 'test.gwy'
         mock_os.path.isfile.return_value = True
-
-        # To prevent real reading of the 'test.gwy' file
         mock_libgwyfile.gwyfile_read_file = Mock(spec=True)
-
         read_gwyfile(filename)
-
         mock_libgwyfile.gwyfile_read_file.assert_called_once_with(
             filename.encode('utf-8'), ANY)
 
