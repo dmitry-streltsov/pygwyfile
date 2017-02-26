444a5380c312bbdb1883568d3ef8d1cdabefd135
Dmitry Streltsov <streltsov.dmitry@gmail.com>
2017-02-26 17:37:58 +0300

    Version 0.1.0a

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name="gwydb",
-      version="0.0.1.a.dev",
+      version="0.1.0a",
       author="Dmitry Streltsov",
       author_email="streltsov.dmitry@gmail.com",
       description=("Project for organizing AFM data files "
@@ -11,11 +11,12 @@ setup(name="gwydb",
       packages=["gwydb"],
       url="https://github.com/dmitry-streltsov/gwy-postgresql",
       classifiers=[
-          "Development Status :: 1 - Planning",
+          "Development Status :: 2 - Pre-Alpha",
           "Intended Audience :: Science/Research",
           "Topic :: Scientific/Engineering :: Physics",
           "License :: OSI Approved :: MIT License",
-          "Programming Language :: Python :: 3"],
+          "Programming Language :: Python :: 3.4",
+          "Operating System :: POSIX :: Linux"],
       setup_requires=["cffi>=1.0.0"],
       cffi_modules=["gwydb/gwy/libgwyfile_build.py:ffibuilder"],
       install_requires=["cffi>=1.0.0", "numpy"]
